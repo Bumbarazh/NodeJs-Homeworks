7f934d54d488a53e25d70915a33023544973da0d
bumbarazh <kos.shynkaruk@gmail.com>
2020-11-30 00:41:57 +0200

    Node Js Lesson Two (CRUD, Handlebars, Eslint)

--- node-lesson-two/app.js
+++ node-lesson-two/app.js
@@ -4,9 +4,15 @@ const express = require('express');
 const expressHbs = require('express-handlebars');
 
 const pathToUsersJsonFile = path.join(process.cwd(), 'views', 'registered-users.json');
+
 let isUserRegistered = false;
 let isEmailInFile = false;
 let isUserEmailInFile = false;
+let isUserLogin = false;
+
+const messageToLogin = 'This email is already register on our site. Please, ';
+const messageToRegister = 'Your email or password aren\'t valid. Please, try to log in again or ';
+const messageToLogOrReg = 'To see yor account, you must to ';
 
 const app = express();
 
@@ -26,19 +32,37 @@ app.set('views', path.join(process.cwd(), 'views'));
 // GET Main page
 
 app.get('/', ((req, res) => {
+    isEmailInFile = false;
+    isUserEmailInFile = false;
+    isUserLogin = false;
+
     res.render('main');
 }));
 
 // GET Login page
 
 app.get('/login', ((req, res) => {
-    res.render('auth', { isEmailInFile });
+    if (isUserRegistered) {
+        return res.redirect('/users');
+    }
+
+    isUserLogin = false;
+    isUserEmailInFile = false;
+
+    res.render('auth');
 }));
 
 // GET Register page
 
 app.get('/register', ((req, res) => {
-    res.render('register', { isUserEmailInFile });
+    if (isUserRegistered) {
+        return res.redirect('/users');
+    }
+
+    isUserLogin = false;
+    isEmailInFile = false;
+
+    res.render('register');
 }));
 
 // GET Users page
@@ -47,69 +71,86 @@ app.get('/users', ((req, res) => {
     if (isUserRegistered) {
         fs.readFile(pathToUsersJsonFile, (err, data) => {
             const users = JSON.parse(data);
+
             res.render('users', { users });
         });
-    } else {
-        res.redirect('/error');
+
+        return;
     }
+
+    isUserLogin = !isUserLogin;
+    res.redirect('/error');
 }));
 
 // Logout
 
 app.get('/logout', ((req, res) => {
     isUserRegistered = !isUserRegistered;
-    isEmailInFile = false;
-    isUserEmailInFile = false;
+
     res.redirect('/');
 }));
 
 // GET Error page
 
-app.get('/error', ((req, res) => {
-    res.render('error');
-}));
+app.get('/error', ((req, res) => res.render('error', {
+    messageToLogin,
+    isUserEmailInFile,
+    messageToRegister,
+    isEmailInFile,
+    messageToLogOrReg,
+    isUserLogin
+})
+));
 
-// POST Login user to App or give him a message
+// POST Login user to App
 
 app.post('/login', ((req, res) => {
     const { email, pass } = req.body;
+
     fs.readFile(pathToUsersJsonFile, (err, data) => {
         if (err) {
             throw err;
         }
+
         const users = JSON.parse(data);
+
         if (users.find((user) => user.email === email && user.pass === pass)) {
             isUserRegistered = !isUserRegistered;
-            res.redirect('/users');
-        } else {
-            isEmailInFile = !isEmailInFile;
-            res.redirect('/login');
+            return res.redirect('/users');
         }
+
+        isEmailInFile = !isEmailInFile;
+        res.redirect('/error');
     });
 }));
 
-// Post register a user in App
+// POST register a user in App
 
 app.post('/register', ((req, res) => {
     const { email, nick, pass } = req.body;
+
     fs.readFile(pathToUsersJsonFile, (err, data) => {
         if (err) {
             throw err;
         }
+
         const users = JSON.parse(data);
+
         if (!users.find((user) => user.email === email)) {
             users.push({ email, pass, nick });
+
             fs.writeFile(pathToUsersJsonFile, JSON.stringify(users), (error) => {
                 if (error) {
                     throw error;
                 }
             });
+
             isUserRegistered = !isUserRegistered;
-            res.redirect('/users');
-        } else {
-            isUserEmailInFile = !isUserEmailInFile;
-            res.redirect('/register');
+            return res.redirect('/users');
         }
+
+        isUserEmailInFile = !isUserEmailInFile;
+        res.redirect('/error');
     });
 }));
 
